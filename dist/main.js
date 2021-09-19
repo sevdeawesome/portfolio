1d58d240ed97a56a3445fd00038d212358584805
Severin Field <sevdeawesome@gmail.com>
2021-09-19 05:19:09 -0700

    added asteroid, background, lorem text

--- dist/main.js
+++ dist/main.js
@@ -12,7 +12,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg')
+  canvas: document.querySelector('#bg'),
+  alpha: true
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -23,15 +24,23 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-// create object
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+// const spaceTexture = new THREE.TextureLoader().load('img/bg.jpg');
+// // spaceTexture.minFilter = THREE.LinearFilter;
+// spaceTexture.magFilter = THREE.NearestFilter;
+// scene.background = spaceTexture;
 
-const torus = new THREE.Mesh(geometry, material);
+let loader = new THREE.TextureLoader();
+loader.load("smoke.png", function(texture){
 
-scene.add(torus);
+})
+scene.fog = new THREE.FogExp2(0x03544e, 0.001);
+renderer.setClearColor(scene.fog.color);
 
-camera.position.x = 5;          
+
+
+
+
+
 
 // LIGHTS
 const pointLight = new THREE.PointLight(0xffffff);
@@ -42,19 +51,68 @@ scene.add(pointLight, ambientLight);
 
 
 
-// HELPERS
+
+const starGeometry = new THREE.SphereGeometry(5,5,5);
+const starMaterial = new THREE.MeshBasicMaterial({color: 0xff0000})
+
+
+const asteroidGeometry = new THREE.SphereGeometry(2, 20, 12);
+const asteroidMaterial = new THREE.MeshBasicMaterial({
+   map: new THREE.TextureLoader().load('img/asteroid2.jpg')
+  // color: 0xff0000
+})
+
+const asteroid = new THREE.Mesh(asteroidGeometry, asteroidMaterial);
+scene.add(asteroid);
+
+
+// const geometry2 = new THREE.BoxGeometry();
+// const material2 = new THREE.MeshStandardMaterial({color: 0xFF6347});
+// const torus = new THREE.Mesh(geometry2, material2);
+// scene.add(torus);
+
+//animate function
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  asteroid.rotation.x += .01;
+  asteroid.rotation.z += .03;
+  // asteroid.scale.x += .1;
+  // camera.rotation.y += .001;
+  
+}
+
+animate();
+
+
+/*  EXAMLPE 3JS HELPERS */
 // const lightHelper = new THREE.PointLightHelper(pointLight);
 // const gridHelper = new THREE.GridHelper() 
 // scene.add(gridHelper, lightHelper);
 
 
- 
-function animate(){
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  torus.rotation.y += .01;
-  // torus.scale.x += .1;
-  
-}
 
-animate();
+
+
+
+
+
+/*  EXAMLPE 3JS CUBE */
+// const geometry2 = new THREE.BoxGeometry();
+// const material2 = new THREE.MeshStandardMaterial({color: 0xFF6347});
+// const torus = new THREE.Mesh(geometry2, material2);
+// scene.add(torus);
+
+
+
+
+
+/*  EXAMLPE 3JS LINE */
+// const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+// const points = [];
+// points.push( new THREE.Vector3( - 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, 10, 0 ) );
+// points.push( new THREE.Vector3( 10, 0, 0 ) );
+// const geometry = new THREE.BufferGeometry().setFromPoints( points );
+// const line = new THREE.Line( geometry, material );
+// scene.add( line );
